4ca7aa0510004584848f02f091e0479ca79b5a4d
D-aniela <daniela.es.ib@gmail.com>
2021-01-06 21:04:33 -0600

    PropTypes and Default Props

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 //Llamara a nuestro mock api para obtener la lista de cursos
 import { getCourses } from "../api/courseApi";
+import CourseList from "./CoursesList";
 
 function CoursesPage() {
   //useState hook, returns an array with two values
@@ -17,27 +18,8 @@ function CoursesPage() {
   //setState llama solo actualiza las propiedades que se especifican
   return (
     <>
-      <h2>Courses</h2>;
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author ID</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>
-          {courses.map((course) => {
-            return (
-              <tr key={course.id}>
-                <td>{course.title}</td>
-                <td>{course.authorId}</td>
-                <td>{course.category}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <h2>Courses</h2>
+      <CourseList courses={courses} />
     </>
   );
 }
